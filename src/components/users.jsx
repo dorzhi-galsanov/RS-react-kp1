319fc0a8f287727c9f04a16f673a5a09db953353
Галсанов Доржи Баирович <GalsanovDB@zrw.oao.rzd>
2023-05-05 11:53:00 +0900

    edit title

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -12,12 +12,14 @@ const Users = () => {
             className = 'badge bg-danger m-2'
             text = 'Никто с тобой не тусанет'
         } else if (length % 100 !== 11 && length % 100 !== 12 && length % 100 !== 13 && length % 100 !== 14 && (length % 10 === 2 || length % 10 === 3 || length % 10 === 4)) {
-            text = `${length} человека тусанет с тобой сегодня`
-        } else {
+            text = `${length} человека тусанут с тобой сегодня`
+        } else if (length === 1) {
             text = `${length} человек тусанет с тобой сегодня`
+        } else {
+            text = `${length} человек тусанут с тобой сегодня`
         }
         return (
-            <span className={className}>{text}</span>
+            <h2><span className={className}>{text}</span></h2>
         )
     }
 
@@ -26,10 +28,9 @@ const Users = () => {
     }
 
     const getAllQualities = (mas) => {
-        let str = mas.reduce((str, quality) => {
-            return <>{str}<span key={quality._id} className={getBadgeClasses(quality.color)}>{quality.name}</span></>
-        }, '')
-        return str
+        return mas.map((quality) => (
+            <span key={quality._id} className={getBadgeClasses(quality.color)}>{quality.name}</span>
+        ))
     }
 
     const handleRemoveUser = (id) => {
